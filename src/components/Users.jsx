fd3571ddbefd4b29b3432d93ff80eb8bb2227993
amdadulhaque78 <amdadulhaque.ier.du@gmail.com>
2025-01-20 01:23:09 +0600

    coffee store client side

---
+++ src/components/Users.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Users = () => {
+
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  const handleDelete = id => {
+    // make sure user is confirm to delete
+    fetch(`http://localhost:5000/user/${id}`, {
+        method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(data => {
+        if(data.deletedCount > 0){
+            console.log('deleted successfully');
+            // remove the user from ui
+            const remainingUsers = users.filter(user => user._id !== id);
+            setUsers(remainingUsers);
+        }
+    })
+  }
+
+  return (
+    <div>
+      <h2>users: {loadedUsers.length} </h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>User Id</th>
+              <th>Email</th>
+              <th>Created At</th>
+              <th>Last Logged In</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                users.map(user => <tr key={user._id}>
+                    <th>1</th>
+                    <td> {user.email} </td>
+                    <td> {user.createdAt} </td>
+                    <td> {user.lastLoggedAt} </td>
+                    <td>
+                        <button onClick={() => handleDelete(user._id)} className="btn">Delete</button>
+                    </td>
+                  </tr>)
+            }
+            
+           
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIm54oM9PO6D8xKwSSng3vTcs7FToIJQk",
+  authDomain: "coffee-store-8061f.firebaseapp.com",
+  projectId: "coffee-store-8061f",
+  storageBucket: "coffee-store-8061f.firebasestorage.app",
+  messagingSenderId: "234170007345",
+  appId: "1:234170007345:web:b594687943be1af9367a10"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
